828fe9907e2418abf874f15de5744b3c5ed68d1d
Amit <amitkvongc2004@gmail.com>
2023-06-26 20:05:28 +0530

    5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React from "react";
 import {BrowserRouter as Router} from 'react-router-dom'
-import { GlobalStyle } from "./GlobalStyle";
-import Hero from './bucket/Hero';
-import Products from './bucket/Products'
-import { productData } from './bucket/Products/info';
-import { productDataTwo } from './bucket/Products/info';
-import Feature from './bucket/Central';
-import Footer from './bucket/Footer';
+//import { GlobalStyle } from "./GlobalStyle";
+// import Hero from './bucket/Hero';
+// import Products from './bucket/Products'
+// import { productData } from './bucket/Products/info';
+// import { productDataTwo } from './bucket/Products/info';
+//import Feature from './bucket/Central';
+///import Footer from './bucket/Footer';
+//import hp from './Update_Main_page/rest.html'
+import i from './Update_Main_page/asset/home-bg.png'
+import j from './Update_Main_page/asset/logo.png'
+import k from './Update_Main_page/asset/nav-bg.png'
 
+ function App() {
+   return (
 
-function App() {
-  return (
-    <Router>
-      <GlobalStyle />
-      <Hero />
-      <Products heading='Your favourite dish' data={productData} />
-      {/* <Feature /> */}
-      <Products heading='Paneer Special' data={productDataTwo} />
-      <Footer />
-    </Router>
-  );
-}
+<> <router>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>IIT-Indore || Night Canteen Portal</title>
+  <link rel="stylesheet" href="./Updat_Main_page/nc.css" />
+  <link rel="icon" href="https://shorturl.at/invCG" />
+  <link href="https://fonts.googleapis.com/css?family=Asap" rel="stylesheet" />
+  <nav id="navbar">
+    <div id="logo">
+      <button onClick={i}> </button> 
+    </div>
+    <ul>
+      <li className="item">
+        <a href="#">Home</a>
+      </li>
+      <li className="item">
+        <a href="#">Menu</a>
+      </li>
+      <li className="item">
+        <a href="#">Orders</a>
+      </li>
+      <li className="item">
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+  </nav>
+  <section id="home">
+    <h1 className="h-primary">IIT-Indore Night Canteen</h1>
+    <p id="p2">
+      The portal caters various needs of the students placing orders and the
+      caterers.
+    </p>
+    <p id="p1">
+      "Midnight Munchies: IIT Indore's Night Canteen for Delicious Bites and
+      Late-Night Cravings! Satisfy Your Hunger in the Late Hours of the Night."
+    </p>
+    <button className="button">
+      <span>ORDER NOW!</span>
+    </button>
+  </section>
+  </router>
+</>
 
+   );
+ }
 export default App;
