9bfc913127890dadb0b15c8b214ad326977acea7
Amit <amitkvongc2004@gmail.com>
2023-06-27 12:05:13 +0530

    17

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { BrowserRouter,Route } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
