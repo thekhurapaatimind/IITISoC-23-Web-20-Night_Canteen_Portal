25c926aba715f11861429346b5bbb12ca2a41511
Amit <amitkvongc2004@gmail.com>
2023-06-27 23:42:06 +0530

    67

--- src/Update_Main_page/Contact/index.js
+++ src/Update_Main_page/Contact/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-
+import Header from '../Header'
 const Contact = () => {
   return (<>
-   <div>ContactUs </div>
+  <Header />
+   <div className='contact_info'>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-lg-10 offset-lg-1'>
+          phone number
+          <div className='contact_info_item'></div>
+          <img src='' alt='phone'/>
+          <div className='contact_info_content'>
+            <div className='contact_info_title'>
+            Phone </div>
+            <div className='contact_info_text'>
+              Phone
+            </div>
+          </div>
+        </div>
+      </div>
+      </div> </div>
     </>
   )
 }
