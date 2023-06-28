1a3e7ed278833f547cef464e67cc0bdc6eeb6696
Amit <amitkvongc2004@gmail.com>
2023-06-28 11:24:51 +0530

    contactUs page

--- src/Update_Main_page/Contact/index.js
+++ src/Update_Main_page/Contact/index.js
@@ -1,26 +1,43 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Header from '../Header'
+import './../Contact/Contact.css'
 const Contact = () => {
   return (<>
   <Header />
-   <div className='contact_info'>
-    <div className='container-fluid'>
-      <div className='row'>
-        <div className='col-lg-10 offset-lg-1'>
-          phone number
-          <div className='contact_info_item'></div>
-          <img src='' alt='phone'/>
-          <div className='contact_info_content'>
-            <div className='contact_info_title'>
-            Phone </div>
-            <div className='contact_info_text'>
-              Phone
-            </div>
-          </div>
-        </div>
+  <section class="ftco-section contact-section bg-light">
+  <div class="container">
+    <div class="row d-flex contact-info">
+      <div class="col-12">
+        <h2 class="h4 font-weight-bold"><span>Contact Information&nbsp;&nbsp; </span></h2>
       </div>
-      </div> </div>
+      <div class="w"></div>
+      <div class="col-3 d-flex">
+       <div class="box">
+         <p><span>Address:</span> Central Dining Facility
+Indian Institute of Technology Indore,
+Khandwa Road, Simrol,
+Indore 453552 </p>
+       </div>
+     </div>
+     <div class="col-3 d-flex">
+       <div class="box">
+         <p><span>Phone :</span> <a href="+91 731-6603368">+ 91 731-6603368</a></p>
+       </div>
+     </div>
+     <div class="col-3 d-flex">
+       <div class="box">
+         <p><span>Email :</span> <a href="mailto:">dining.committee.sg@iiti.ac.in</a></p>
+       </div>
+     </div>
+     <div class="col-3 d-flex">
+       <div class="box">
+         <p><span>Website :</span> <a href="#">http://localhost:3000/</a></p>
+       </div>
+     </div>
+   </div>
+ </div>
+</section>
     </>
   )
 }
