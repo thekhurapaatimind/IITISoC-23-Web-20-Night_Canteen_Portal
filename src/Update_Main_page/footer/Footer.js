0bf0296f02366cb0ece80494ca4225db9376b1a6
Akshat <akshatgopal70@gmail.com>
2023-07-04 11:51:36 +0530

    FOOTER COMPLETED

--- src/Update_Main_page/footer/Footer.js
+++ src/Update_Main_page/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./footerS.css";
 import {
   Box,
   Container,
@@ -20,20 +21,20 @@ const Footer = () => {
    <Box>
   < Container>
   <Htext>
-    MADE WITH LOVE BY IIT INDORE TEAM<br/>
+    MADE WITH LOVE <span style={{color:'red'}}>&#10084;</span> BY IITISoC WEB20 TEAM<br/>
     </Htext>
-    <Rr>All Rights Reserved &#174;</Rr>
+    <Rr>Copyright &#169; 2023</Rr>
   
   <Lnk>
-    <ul>
+    <ul style={{display:'flex',listStyle:'none'}}>
       <li>
-<a href="http://twitter.com" rel="me"><img title=" Twitter" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/512px-Logo_of_Twitter.svg.png?20220821125553" alt="Twitter Button" width="18" height="18" /></a>
+<a href="http://diningfee.iiti.ac.in/" ><img src="https://upload.wikimedia.org/wikipedia/en/c/c4/Indian_Institute_of_Technology%2C_Indore_Logo.png" alt="IIT-I" title="IIT-I" width="25" height="25"></img></a>
       </li>
       <li>
-<a href="https://www.instagram.com/iitindoreofficial/?hl=en" ><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/800px-Instagram_logo_2022.svg.png" alt="Insta" title="Instagram" width="18" height="18"></img></a>
+<a href="https://www.instagram.com/iitindoreofficial/?hl=en" ><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/800px-Instagram_logo_2022.svg.png" alt="Insta" title="Instagram" width="25" height="25"></img></a>
       </li>
       <li>
-<a href="http://diningfee.iiti.ac.in/" ><img src="https://upload.wikimedia.org/wikipedia/en/c/c4/Indian_Institute_of_Technology%2C_Indore_Logo.png" alt="IIT-I" title="IIT-I" width="18" height="18"></img></a>
+<a href="http://twitter.com" rel="me"><img title=" Twitter" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/512px-Logo_of_Twitter.svg.png?20220821125553" alt="Twitter Button" width="25" height="25" /></a>
       </li>
     </ul>
     </Lnk>
