addb4ab6c2583b945ebe3e2ba38924257759824a
Akshat <akshatgopal70@gmail.com>
2023-07-04 12:57:31 +0530

    Orders

---
+++ src/Update_Main_page/Orders/func.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Header from '../Header';
+import { NavLink } from 'react-router-dom';
+import{
+    OrdersHeading
+} from './Orders'
+
+
+function Orders(){
+    const navigate=useNavigate();
+    return(
+        <>
+        <Header/>
+        <OrdersHeading>
+            My Orders
+        </OrdersHeading>
+        
+        </>
+    )
+
+}
+export default Orders
