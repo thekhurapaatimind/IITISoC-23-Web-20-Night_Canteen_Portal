4bc5a4c7952a41465c64297e73e9579e2307ed28
Akshat <akshatgopal70@gmail.com>
2023-07-13 12:55:17 +0530

    466

--- src/App.js
+++ src/App.js
@@ -12,25 +12,25 @@ import Orders from "./Update_Main_page/Orders/Orders";
 
 import styled from "styled-components";
  function App() {
-  const isMobileDevice = useMediaQuery({
-    query: "(min-device-width: 480px)",
-  });
+  // const isMobileDevice = useMediaQuery({
+  //   query: "(min-device-width: 480px)",
+  // });
 
-  const isTabletDevice = useMediaQuery({
-    query: "(min-device-width: 768px)",
-  });
+  // const isTabletDevice = useMediaQuery({
+  //   query: "(min-device-width: 768px)",
+  // });
 
-  const isLaptop = useMediaQuery({
-    query: "(min-device-width: 1024px)",
-  });
+  // const isLaptop = useMediaQuery({
+  //   query: "(min-device-width: 1024px)",
+  // });
 
-  const isDesktop = useMediaQuery({
-    query: "(min-device-width: 1200px)",
-  });
+  // const isDesktop = useMediaQuery({
+  //   query: "(min-device-width: 1200px)",
+  // });
 
-  const isBigScreen = useMediaQuery({
-    query: "(min-device-width: 1201px )",
-  });
+  // const isBigScreen = useMediaQuery({
+  //   query: "(min-device-width: 1201px )",
+  // });
   return (
    <>
 <Routes>
